3c90a1a91bf1c2fc87bb515afe975ad51e3b213f
Peter Pagac <QuarKUS7@gmail.com>
2019-02-24 02:42:25 +0100

    Procfile added

--- api/app.py
+++ api/app.py
@@ -46,4 +46,5 @@ def classification(result):
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
+   port = int(os.environ.get('PORT', 5000))
    app.run(host='0.0.0.0')
