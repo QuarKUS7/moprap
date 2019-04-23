e90fd1e9cdd4a6cb233651baabf7e26ebc5ac22d
Peter Pagac <QuarKUS7@gmail.com>
2019-04-23 19:57:38 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -41,4 +41,4 @@ def classification(result):
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+   app.run(host='0.0.0.0', port=8000)
