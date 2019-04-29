9c283e39a240163299ab34f9938b5c108a0f27b4
Peter Pagac <QuarKUS7@gmail.com>
2019-04-29 23:49:23 +0200

    working local

--- api/model_download.sh
+++ api/model_download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fileid="1QBZh4aZOhJdxH3rGgaf0JdlwyTJQd2YE"
+fileid="1SCKFTEVHB-jRKXanqap6f7ErV1JXM32v"
 filename="fk.pth"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
